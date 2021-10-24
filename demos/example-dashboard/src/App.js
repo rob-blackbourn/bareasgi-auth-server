6a9ae6417382c1dcff8d9258ac498687f1e41046
Rob Blackbourn <rob.blackbourn@gmail.com>
2021-10-24 09:10:13 +0100

    Moved theme up

--- demos/example-dashboard/src/App.js
+++ demos/example-dashboard/src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
 
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
 import { AuthenticatedApp } from './components'
 import Site from './Site'
 import config from './config'
 
+const theme = createTheme()
+
 class App extends Component {
   render() {
     return (
-      <AuthenticatedApp
-        loginPath={config.loginPath}
-        whoamiPath={config.whoamiPath}
-        renderer={props => <Site {...props} />}
-      />
+      <ThemeProvider theme={theme}>
+        <AuthenticatedApp
+          loginPath={config.loginPath}
+          whoamiPath={config.whoamiPath}
+          renderer={props => <Site {...props} />}
+        />
+      </ThemeProvider>
     )
   }
 }
--- demos/example-dashboard/src/components/DashboardRouter/DashboardRouter.js
+++ demos/example-dashboard/src/components/DashboardRouter/DashboardRouter.js
@@ -2,8 +2,6 @@ import * as React from 'react'
 import PropTypes from 'prop-types'
 import { BrowserRouter, Link as BrowserLink } from 'react-router-dom'
 
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import CssBaseline from '@mui/material/CssBaseline'
@@ -24,8 +22,6 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import StyledAppBar from './StyledAppBar'
 import StyledDrawer from './StyledDrawer'
 
-const mdTheme = createTheme()
-
 export default class DashboardRouter extends React.Component {
   state = {
     open: false
@@ -42,124 +38,122 @@ export default class DashboardRouter extends React.Component {
 
     return (
       <BrowserRouter basename={basename}>
-        <ThemeProvider theme={mdTheme}>
-          <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
 
-            <StyledAppBar position="absolute" open={open}>
-              <Toolbar
-                sx={{
-                  pr: '24px' // keep right padding when drawer closed
-                }}
-              >
-                <IconButton
-                  edge="start"
-                  color="inherit"
-                  aria-label="open drawer"
-                  onClick={this.handleToggleDrawer}
-                  sx={{
-                    marginRight: '36px',
-                    ...(open && { display: 'none' })
-                  }}
-                >
-                  <MenuIcon />
-                </IconButton>
-
-                <Typography
-                  component="h1"
-                  variant="h6"
-                  color="inherit"
-                  noWrap
-                  sx={{ flexGrow: 1 }}
-                >
-                  {title}
-                </Typography>
-
-                {appBarItems && appBarItems}
-              </Toolbar>
-            </StyledAppBar>
-
-            <StyledDrawer variant="permanent" open={open}>
-              <Toolbar
-                sx={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'flex-end',
-                  px: [1]
-                }}
-              >
-                <IconButton onClick={this.handleToggleDrawer}>
-                  <ChevronLeftIcon />
-                </IconButton>
-              </Toolbar>
-
-              <Divider />
-
-              <List>
-                {applications.map(application => (
-                  <Tooltip
-                    placement="right"
-                    title={
-                      <Typography variant="subtitle2" color="inherit">
-                        {application.description}
-                      </Typography>
-                    }
-                    key={application.code}
-                  >
-                    <ListItem button component="a" href={application.url}>
-                      <ListItemIcon>
-                        <Icon>{application.iconName}</Icon>
-                      </ListItemIcon>
-                      <ListItemText primary={application.title} />
-                    </ListItem>
-                  </Tooltip>
-                ))}
-              </List>
-
-              <Divider />
-
-              <List>
-                {links.map(link => (
-                  <Tooltip
-                    placement="right"
-                    title={
-                      <Typography variant="subtitle2" color="inherit">
-                        {link.description}
-                      </Typography>
-                    }
-                    key={link.code}
-                  >
-                    <ListItem button component={BrowserLink} to={link.path}>
-                      <ListItemIcon>
-                        <Icon>{link.iconName}</Icon>
-                      </ListItemIcon>
-                      <ListItemText sx={{ m: 0, p: 0 }} primary={link.title} />
-                    </ListItem>
-                  </Tooltip>
-                ))}
-              </List>
-            </StyledDrawer>
-
-            <Box
-              component="main"
+          <StyledAppBar position="absolute" open={open}>
+            <Toolbar
               sx={{
-                backgroundColor: theme =>
-                  theme.palette.mode === 'light'
-                    ? theme.palette.grey[100]
-                    : theme.palette.grey[900],
-                flexGrow: 1,
-                height: '100vh',
-                overflow: 'auto'
+                pr: '24px' // keep right padding when drawer closed
               }}
             >
-              <Toolbar />
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={this.handleToggleDrawer}
+                sx={{
+                  marginRight: '36px',
+                  ...(open && { display: 'none' })
+                }}
+              >
+                <MenuIcon />
+              </IconButton>
 
-              <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-                {routes}
-              </Container>
-            </Box>
+              <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}
+              >
+                {title}
+              </Typography>
+
+              {appBarItems && appBarItems}
+            </Toolbar>
+          </StyledAppBar>
+
+          <StyledDrawer variant="permanent" open={open}>
+            <Toolbar
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                px: [1]
+              }}
+            >
+              <IconButton onClick={this.handleToggleDrawer}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </Toolbar>
+
+            <Divider />
+
+            <List>
+              {applications.map(application => (
+                <Tooltip
+                  placement="right"
+                  title={
+                    <Typography variant="subtitle2" color="inherit">
+                      {application.description}
+                    </Typography>
+                  }
+                  key={application.code}
+                >
+                  <ListItem button component="a" href={application.url}>
+                    <ListItemIcon>
+                      <Icon>{application.iconName}</Icon>
+                    </ListItemIcon>
+                    <ListItemText primary={application.title} />
+                  </ListItem>
+                </Tooltip>
+              ))}
+            </List>
+
+            <Divider />
+
+            <List>
+              {links.map(link => (
+                <Tooltip
+                  placement="right"
+                  title={
+                    <Typography variant="subtitle2" color="inherit">
+                      {link.description}
+                    </Typography>
+                  }
+                  key={link.code}
+                >
+                  <ListItem button component={BrowserLink} to={link.path}>
+                    <ListItemIcon>
+                      <Icon>{link.iconName}</Icon>
+                    </ListItemIcon>
+                    <ListItemText sx={{ m: 0, p: 0 }} primary={link.title} />
+                  </ListItem>
+                </Tooltip>
+              ))}
+            </List>
+          </StyledDrawer>
+
+          <Box
+            component="main"
+            sx={{
+              backgroundColor: theme =>
+                theme.palette.mode === 'light'
+                  ? theme.palette.grey[100]
+                  : theme.palette.grey[900],
+              flexGrow: 1,
+              height: '100vh',
+              overflow: 'auto'
+            }}
+          >
+            <Toolbar />
+
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+              {routes}
+            </Container>
           </Box>
-        </ThemeProvider>
+        </Box>
       </BrowserRouter>
     )
   }
